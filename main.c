1779b5ef91ae756bd65737227ec591cadd578937
yux20000304 <2056404413@qq.com>
2022-10-25 22:38:41 +0800

    add timer

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include "hashmap.h"
+#include <sys/time.h>
 
 int main(void)
 {
     HashMap *hm = new_hashmap();
 
+    struct timeval start,end;
+    gettimeofday(&start, NULL);
     for (int i = 0, j = 10000; i < 10000; i++, j--)
     {
         if (hashmap_insert(hm, i, j))
         {
-            fprintf(stderr, "Error on inserting %d:%d\n", i, j);
+            // fprintf(stderr, "Error on inserting %d:%d\n", i, j);
         }
     }
+    gettimeofday(&end, NULL);
+    long timeuse = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    printf("time=%f\n",timeuse / 1000000.0);
 
     for (int i = 2500; i < 7500; i++)
     {
         if (hashmap_remove(hm, i))
         {
-            fprintf(stderr, "Error, key %d not in hashmap\n", i);
+            // fprintf(stderr, "Error, key %d not in hashmap\n", i);
         }
     }
 
@@ -25,7 +31,7 @@ int main(void)
     for (int i = 0; i < 10000; i++)
     {
         int err = hashmap_get(hm, i, &res);
-        printf("%d:%d\n", i, err ? -1 : res);
+        // printf("%d:%d\n", i, err ? -1 : res);
         res = 0;
     }
 
